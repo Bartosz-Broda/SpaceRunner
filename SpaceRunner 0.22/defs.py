673f978309dfd150ec7ff947da177b7a2d034f3f
Bartosz-Broda <66310785+Bartosz-Broda@users.noreply.github.com>
2021-04-02 20:07:11 +0200

    Add files via upload

---
+++ SpaceRunner 0.22/defs.py
@@ -0,0 +1,23 @@
+survive_time = 80
+
+num_planets = 28
+num_rocks = 99
+
+moment = 'inf'  # a.k.a. rotational mass, not to be confused with momentum ("rotational speed")
+mass = 10
+elasticity = 0.5
+friction = 0.5
+
+planet_moment = 'inf'
+planet_mass = 1000
+rock_mass = 10
+wall_elasticity = 0.01
+wall_friction = 1.0
+
+
+default_collision_type = 1
+lose_collision_type = 2
+ship_collision_type = 3
+planet_collision_type = 4
+rock_collision_type = 0
+star_collision_type = 5
